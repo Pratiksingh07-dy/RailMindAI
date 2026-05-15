e81e8a0be8c935514e40672fef8b9f56cfd45bc6
Pratiksingh07-dy <pratiksingh2205@gmail.com>
2026-05-15 22:07:37 +0530

    Integrated PostgreSQL and created User model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from app.routes import health
 
+from app.database.database import engine, Base
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 app.include_router(health.router)
