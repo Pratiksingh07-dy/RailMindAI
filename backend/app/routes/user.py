9116ac85c794f1bc1e1a0c5013c709011e6ea7e6
Pratiksingh07-dy <pratiksingh2205@gmail.com>
2026-05-16 16:30:12 +0530

    Implemented JWT authentication system

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -4,15 +4,27 @@ from sqlalchemy.orm import Session
 from app.database.database import get_db
 from app.models.user import User
 from app.schemas.user_schema import UserCreate
-from app.utils.security import hash_password
+from app.schemas.login_schema import LoginRequest
 
-router = APIRouter(prefix="/users", tags=["Users"])
+from app.utils.security import (
+    hash_password,
+    verify_password
+)
+
+from app.utils.auth import create_access_token
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
 
 @router.post("/signup")
-def signup(user: UserCreate, db: Session = Depends(get_db)):
+def signup(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
 
-
-    
     new_user = User(
         username=user.username,
         email=user.email,
@@ -23,8 +35,40 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
 
-
     return {
         "message": "User created successfully",
         "user_id": new_user.id
+    }
+
+
+@router.post("/login")
+def login(
+    user: LoginRequest,
+    db: Session = Depends(get_db)
+):
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not existing_user:
+        return {
+            "message": "User not found"
+        }
+
+    if not verify_password(
+        user.password,
+        existing_user.password
+    ):
+        return {
+            "message": "Incorrect password"
+        }
+
+    token = create_access_token(
+        {"sub": existing_user.email}
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
     }
